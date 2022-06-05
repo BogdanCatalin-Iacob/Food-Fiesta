dd7c22c58b1440d42af377c469c0237e38ec5c2e
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-06-05 07:38:33 +0000

    Create SQL db models

---
+++ food_fiesta/models.py
@@ -0,0 +1,22 @@
+from food_fiesta import db
+
+
+class Category(db.Model):
+    '''schema for the Category model'''
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+
+    def __repr__(self):
+        '''return itself as a string'''
+        return self.category_name
+
+
+class Users(db.Model):
+    '''schema for the Users model'''
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        '''return itself as a string'''
+        return self.user_name
