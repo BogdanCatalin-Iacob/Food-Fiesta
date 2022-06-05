2efbee659cffa5e7dd729c9a8823fa14d2e62f41
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-06-05 08:55:45 +0000

    Import Pymongo and config the app to use it

--- food_fiesta/__init__.py
+++ food_fiesta/__init__.py
@@ -1,6 +1,7 @@
 import os
 import re
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env # noqa
@@ -10,7 +11,11 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 # routes rely on app and db variables therefore must be imported after these
 from food_fiesta import routes # noqa
