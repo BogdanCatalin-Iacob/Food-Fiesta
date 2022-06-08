463d2f993bff2e52925dfb37f1cedca4f61511be
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-06-08 17:01:57 +0000

    Initialize the collapsible element on recipes.html page

--- food_fiesta/static/js/script.js
+++ food_fiesta/static/js/script.js
@@ -1,5 +1,9 @@
-// mobile nav initialization
 document.addEventListener('DOMContentLoaded', function() {
+  // mobile nav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    //collapsible component initialization
+    let elems = document.querySelectorAll('.collapsible');
+    let instances = M.Collapsible.init(elems);
   });
