bfbab0a4c9849851f6224174a813b23620b7776d
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-06-09 19:10:30 +0000

    Add totalTime live update functionality

--- food_fiesta/static/js/script.js
+++ food_fiesta/static/js/script.js
@@ -1,9 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // mobile nav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
 
-    //collapsible component initialization
-    let elems = document.querySelectorAll('.collapsible');
-    let instances = M.Collapsible.init(elems);
-  });
+  // collapsible component initialization (recipes)
+  let elems = document.querySelectorAll('.collapsible');
+  let instances = M.Collapsible.init(elems);
+
+  // select list initialization (create_recipe)
+  let list_elems = document.querySelectorAll('select');
+  let list_instances = M.FormSelect.init(list_elems);
+  totalTime();
+});
+
+/**
+ * update the create_recipe form total time by adding cook_time and prep_time
+ */
+function totalTime() {
+  // get the values from the form
+  let cookTime = parseInt(document.getElementById("cook_time").value);
+  let prepTime = parseInt(document.getElementById("prep_time").value);
+  let totalTime = 0;
+
+  // check if is any value in the fields
+  if (cookTime >= 0 && prepTime >= 0) {
+    totalTime = cookTime + prepTime;
+  } else if (cookTime >= 0) {
+    totalTime = cookTime;
+  } else if (prepTime >= 0) {
+    totalTime = prepTime;
+  }
+
+  document.getElementById("total_time").value = totalTime;
+}
