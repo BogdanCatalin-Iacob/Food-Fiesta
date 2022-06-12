6e3042d27dab3ffb11a76c6c27d8bad2939d0e44
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-06-12 00:40:46 +0000

    Add preparation steps to list

--- food_fiesta/static/js/script.js
+++ food_fiesta/static/js/script.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // collapsible component initialization (recipes)
   let elems = document.querySelectorAll('.collapsible');
-  let instances = M.Collapsible.init(elems);
+  M.Collapsible.init(elems);
 
   // select list initialization (create_recipe)
   let list_elems = document.querySelectorAll('select');
-  let list_instances = M.FormSelect.init(list_elems);
+  M.FormSelect.init(list_elems);
   totalTime();
 });
 
@@ -37,10 +37,10 @@ function totalTime() {
 /**
  * Add ingredients to the list and display them
  */
-function addIngredient(){
+function addIngredient() {
   let ingredientsList = document.getElementById("ingredientsList");
   // show the field if hidden
-  if (ingredientsList.classList.contains("hide")){
+  if (ingredientsList.classList.contains("hide")) {
     ingredientsList.classList.remove("hide");
   }
 
@@ -51,9 +51,35 @@ function addIngredient(){
   document.getElementById("ingredients").value = "";
 
   // display all the items from the ingredients list
-  ingredients.forEach(item => {document.getElementById("ingredientsList").innerHTML += `<input class="col s11 m4 l3" type="text" value="${item}">`});
+  ingredients.forEach(item => {
+    document.getElementById("ingredientsList").innerHTML += `<input disabled class="col s12 m4 l3" type="text" value="${item}">`
+  });
+}
+let stepCount = 1;
+
+function addStep() {
+  let stepList = document.getElementById("stepsList");
+  // show the firld if hidden
+  if (stepList.classList.contains("hide")) {
+    stepList.classList.remove("hide");
+  }
+
+  let stepsList = {}
+  let stepValue = document.getElementById("steps").value;
+  stepsList[stepCount] = stepValue;
+  document.getElementById("steps").value = "";
+  stepCount++;
+  document.getElementById("prep_step").innerText = `Preparation step ${stepCount}`;
+
+  for (step in stepsList) {
+    document.getElementById("stepsList").innerHTML += `<p class="col s2">Step ${step}: 
+      </p><textarea disabled class="materialize-textarea">${stepsList[step]}</textarea>`
+  }
 }
 
-// add ingredient button event listener
+// add ingredient and steps buttons event listeners
 let addButton = document.getElementById("addButton");
+let addStepButton = document.getElementById("addStepButton");
+
 addButton.addEventListener("click", addIngredient);
+addStepButton.addEventListener("click", addStep);
